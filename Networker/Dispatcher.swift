cbc35ef7570c21f83a25dc93b8b95206b157524c
Alexander Belyavskiy <diejmon@gmail.com>
2019-05-07 17:58:22 +0300

    Reformat

--- Networker/Dispatcher.swift
+++ Networker/Dispatcher.swift
@@ -8,11 +8,8 @@ public protocol Dispatcher: AnyObject {
 }
 
 extension URLSession: Dispatcher {
-  public func dispatch<Success, Decoder>(_ request: Request<Success, Decoder>,
-                                         completionQueue: DispatchQueue,
-                                         completion: @escaping (Result<Success, Decoder.ErrorType>) -> Void)
-    where Success: Decodable, Decoder: ResponseDecoder
-  {
-    
-  }
+  public func dispatch<Success, Decoder>(_: Request<Success, Decoder>,
+                                         completionQueue _: DispatchQueue,
+                                         completion _: @escaping (Result<Success, Decoder.ErrorType>) -> Void)
+    where Success: Decodable, Decoder: ResponseDecoder {}
 }
