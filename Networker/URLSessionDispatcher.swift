eeb8ec74880ea09a74f095de7eb086bb6fa09d34
Aliaksandr Bialiauski <Aliaksandr.Bialiauski@darkmatter.ae>
2019-05-10 18:30:35 +0300

    URLSession and auth plugin to URLSessionDispatcher

--- Networker/URLSessionDispatcher.swift
+++ Networker/URLSessionDispatcher.swift
@@ -1,6 +1,37 @@
 import Foundation
 
-public class URLSessionDispatcher {
+public class URLSessionDispatcher: NSObject {
+  private struct URLSessionInitData {
+    let urlSessionConfiguration: URLSessionConfiguration
+    let delegateQueue: OperationQueue?
+  }
+
+  private var urlSessionInitData: URLSessionInitData?
+  private lazy var urlSession: URLSession = {
+    guard let urlSessionInitData = self.urlSessionInitData else {
+      preconditionFailure()
+    }
+
+    let operationQueue = OperationQueue()
+    let urlSession = URLSession(configuration: urlSessionInitData.urlSessionConfiguration,
+                                delegate: self,
+                                delegateQueue: urlSessionInitData.delegateQueue)
+    self.urlSessionInitData = nil
+    return urlSession
+  }()
+
+  private var rwAtomicPlugins: RWAtomic<[DispatcherPlugin]>
+
+  public init(plugins: [DispatcherPlugin],
+              urlSessionConfiguration: URLSessionConfiguration = .default,
+              urlSessionDelegateQueue: OperationQueue?) {
+    urlSessionInitData = URLSessionInitData(urlSessionConfiguration: urlSessionConfiguration,
+                                            delegateQueue: urlSessionDelegateQueue)
+    rwAtomicPlugins = RWAtomic(plugins)
+  }
+}
+
+extension URLSessionDispatcher: Dispatcher {
   public var plugins: [DispatcherPlugin] {
     get {
       return rwAtomicPlugins.value
@@ -18,14 +49,6 @@ public class URLSessionDispatcher {
     }
   }
 
-  private var rwAtomicPlugins: RWAtomic<[DispatcherPlugin]>
-
-  public init(plugins: [DispatcherPlugin]) {
-    rwAtomicPlugins = RWAtomic(plugins)
-  }
-}
-
-extension URLSessionDispatcher: Dispatcher {
   public func prepareUrlRequest<Success, Decoder>(_ request: Request<Success, Decoder>) -> URLRequest {
     let urlRequest = URLRequest(url: request.url,
                                 cachePolicy: request.cachePolicy,
@@ -37,11 +60,34 @@ extension URLSessionDispatcher: Dispatcher {
                                                      requestType: Request<Success, Decoder>.Type,
                                                      completionQueue: DispatchQueue,
                                                      completion: @escaping (Result<Success, Decoder.ErrorType>) -> Void) {
-    URLSession.shared.dataTask(with: urlRequest) { data, response, error in
+    let urlSessionTask = urlSession.dataTask(with: urlRequest) { data, response, error in
       let result = requestType.convert(data: data, response: response, error: error)
       completionQueue.async {
         completion(result)
       }
+    }
+
+    urlSessionTask.resume()
+  }
+}
+
+extension URLSessionDispatcher: URLSessionTaskDelegate {
+  public func urlSession(_: URLSession,
+                         task _: URLSessionTask,
+                         didReceive challenge: URLAuthenticationChallenge,
+                         completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Void) {
+    var challengeHandled: Bool = false
+
+    for plugin in plugins {
+      challengeHandled = plugin.fullFill(challenge: challenge,
+                                         completion: completionHandler)
+      if challengeHandled {
+        break
+      }
+    }
+
+    if !challengeHandled {
+      completionHandler(.performDefaultHandling, nil)
     }
   }
 }
