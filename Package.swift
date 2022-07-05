fb79543f07dd3e434adacdcd778bafc32e12e5fc
Aliaksandr Bialiauski <aliaksandr@bialiauski.com>
2022-07-05 11:25:51 +0300

    Bump Xcode requirement and lower iOS requirement

--- Package.swift
+++ Package.swift
@@ -1,14 +1,15 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.6
 
 import PackageDescription
 
 let package = Package(
   name: "Networker",
-  platforms: [.macOS(.v12), .iOS(.v15)],
+  platforms: [.macOS(.v12), .iOS(.v14)],
   products: [
     .library(
       name: "Networker",
-      targets: ["Networker"]),
+      targets: ["Networker"]
+    ),
   ],
   dependencies: [
     .package(url: "https://github.com/apple/swift-log.git", from: "1.4.0")
